73921e6a788e26d52b618a200b30b8732f55b476
NEB-H <mohamednebbache@yahoo.fr>
2021-07-31 18:21:21 -0500

    ajout choix option pages product

--- product.js
+++ product.js
@@ -13,6 +13,7 @@ fetch("http://localhost:3000/api/cameras/"+id)
                         let price = listProduct.price;
                         let description = listProduct.description;
                         let imgUrl=listProduct.imageUrl;
+                        let lenses= listProduct.lenses
                         
     /*locatlisation Article*/let locArticle = document.getElementsByTagName('article');
     /*fonions creer img*/    let newImg = document.createElement("img"); 
@@ -20,12 +21,17 @@ fetch("http://localhost:3000/api/cameras/"+id)
     /*localisation img*/     let locImg= document.getElementById(id);
     /*localisation title*/   let locTitle= document.getElementById("title"); 
     /*locdescription*/       let locdescription= document.getElementById("description");
-                             let locPrice= document.getElementById("price");
-
+    /*locprice*/             let locPrice= document.getElementById("price");
+    /*locoption*/            let locOption= document.getElementById("lensSelect")
         /*config img */          locImg.setAttribute('src',imgUrl);
                                  locImg.setAttribute('width', 150);
                                  locImg.setAttribute('alt', "Apareil photo");
         /*config title */        locTitle.innerHTML= name;
         /*config Description */  locdescription.innerHTML= description;
                                  locPrice.innerHTML= price+"$";
+                                 test=lenses.length
+                                 
+    /*repeter nombre de lenses*/ for(let i=0;i < lenses.length;i++){
+    /*Ajout d'option*/                 locOption.add (new Option(lenses[i]));}
 })
+
